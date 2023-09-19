baa2c97155881d20327d64fae9d7839d86291bf9
charles.chen@databricks.com <charles.chen@databricks.com>
2023-09-19 03:10:57 +0000

    csc 2023-09-18 20-10

---
+++ tests/test_modules.py.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# import pytest
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+row_count = 11
+
+str_schema = "StructType([StructField('MedInc', DoubleType(), True), StructField('HouseAge', LongType(), True), StructField('AveRooms', DoubleType(), True), StructField('AveBedrms', DoubleType(), True), StructField('Population', LongType(), True), StructField('AveOccup', DoubleType(), True), StructField('Latitude', DoubleType(), True), StructField('Longitude', DoubleType(), True), StructField('MedHouseVal', DoubleType(), True)])"
+# df = spark.read.table('default.sklearn_housing')
+# assert df.count() == row_count
+# assert str(df.schema) == str_schema
+
+table_input_df = spark.read.table('hive_metastore.default.sklearn_housing')
+
+# @pytest.fixture
+# def table_input_df():
+#     return spark.read.table('default.sklearn_housing')
+
+def test_table_row_count():  
+    assert table_input_df.count() == row_count
+
+def test_table_schema():  
+    assert str(table_input_df.schema) == str_schema
+
