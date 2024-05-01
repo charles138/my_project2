2ccf4220ec7629e9ada8970c275c48f76419b220
Charles Chen <charles138@gmail.com>
2024-04-30 21:20:07 -0700

    csc 2024-04-30 21:17: others

--- events/belt_speed/__init__.py
+++
@@ -1,8 +0,0 @@
-BELT_SPEED_SP = 'BeltSpdSP'
-BELT_DRV_RUN_CMD = 'BeltDrvRunCmd'
-BELT_SPEED_EST = 'BeltSpdEst'
-
-belt_speed_tags = [
-    BELT_SPEED_SP,
-    BELT_DRV_RUN_CMD,
-]
--- events/climate_state/__init__.py
+++
@@ -1,5 +0,0 @@
-CLS_EVAP_FAN_STATUS = 'EvapAnyStatus'
-
-climate_system_state_tags = [
-    CLS_EVAP_FAN_STATUS,
-]
